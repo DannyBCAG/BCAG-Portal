6ee96065a1cf0310d8f53958c206e15b0b38974d
DannyBCAG <broughcag@gmail.com>
2026-09-14 11:40:23 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,13 @@
+{
+  "name": "BCAG Portal",
+  "short_name": "BCAG",
+  "description": "Brough Community Art Group portal",
+  "start_url": "./",
+  "display": "standalone",
+  "background_color": "#ffffff",
+  "theme_color": "#0b4da0",
+  "icons": [
+    {"src": "icon-192.png","sizes": "192x192","type": "image/png"},
+    {"src": "icon-512.png","sizes": "512x512","type": "image/png"}
+  ]
+}
